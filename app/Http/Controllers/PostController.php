3b0fd3f82c35bdac71b591a0597001cd9f78782a
victor <victorhvercosag@gmail.com>
2025-03-11 10:19:32 -0300

    crud básico finalizado

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,22 +8,38 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function create(Request $request) {
-        $new_post = [
-            'title' => 'Meu primeiro post',
-            'content' => 'Conteúdo qualquer',
-            'author' => 'Alessandro'
-        ];
-
-        // forma mais convencional de criar um registro no banco. 
-        // $post = new Post($new_post);
-        // $post->save();
-
         $post = new post();
 
         $post->title = 'Meu primeiro post';
         $post->content = 'Conteúdo dualquer';
         $post->author = 'Elassondra';
+        $post->save();
+    }
+    public function read(Request $r) {
+        $post = new Post();
 
-        dd($post);
+        $post = $post->find(1); // posso usar where(); ou find();
+                        // find sempre procura a chave primária
+
+        return $post;
+    }
+
+    public function all(Request $r) {
+        $posts = Post::all();
+
+        return $posts;
+    }
+
+    public function update(Request $request) {
+        $posts = Post::where('id','>',0)->update([
+            'author' => 'Desconhecido'
+        ]);
+
+        return $posts;
+    }
+
+    public function delete(Request $request) {
+        $post = Post::where('id','>',0)->delete();
+        return $post;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/posts/create', [PostController::class,'create']);
+Route::get('/posts/read', [PostController::class,'read']);
+Route::get('/posts/all', [PostController::class,'all']);
+Route::get('/posts/update', [PostController::class,'update']);
+Route::get('/posts/delete', [PostController::class,'delete']);
